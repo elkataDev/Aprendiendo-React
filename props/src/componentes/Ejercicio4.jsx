d8fbce61541c4728a65023f439f763c0c6907d53
Antonio Espinola Ramirez <antonioespinola344@gmail.com>
2026-05-05 17:54:09 +0200

    Ejercicio 4

--- props/src/componentes/Ejercicio4.jsx
+++ props/src/componentes/Ejercicio4.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Ejercicio4() {
+  // El estado inicial debe ser un array vacio
+  const [posts, setPosts] = useState([]);
+
+  const cargarPost = async () => {
+    const nuevosPosts = [];
+
+    for (let i = 1; i <= 5; i++) {
+      try {
+        const respuesta = await fetch(`https://jsonplaceholder.typicode.com/posts/${i}`);
+        if (!respuesta.ok) throw new Error("Error al cargar");
+        
+        const datos = await respuesta.json();
+        nuevosPosts.push(datos);
+      } catch (error) {
+        console.error("No se cargó el post:", error);
+      }
+    }
+
+    // Actualizamos el estado
+    setPosts(nuevosPosts);
+  };
+
   return (
-    <div>Ejercicio4</div>
-  )
+    <>
+      <button onClick={cargarPost}>Generar 5 posts</button> 
+      
+      <ul>
+        {/* Usamos map para mostrar la lista */}
+        {posts.map((post) => (
+          <li>
+            <strong>{post.title}</strong>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
-export default Ejercicio4
+export default Ejercicio4;
