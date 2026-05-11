3ad04f3df6559c24cddb9372d5b77bf7ad71f1ae
Antonio Espinola Ramirez <antonioespinola344@gmail.com>
2026-05-11 18:35:33 +0200

    Comentarios rutas dinamicas y anidadas

--- rutas/rutas/src/Pages/Ordenadores.jsx
+++ rutas/rutas/src/Pages/Ordenadores.jsx
@@ -7,7 +7,7 @@ function Ordenadores() {
             <h2>Zotac Tech</h2>
             {/* Cargamos todos los Productos */}
             <main className='tienda-content'>
-                <Outlet />
+                <Outlet /> {/* Aquí es donde deben aparecerán los componentes de las rutas hijas (ListaProducto y DetalleProducto) */}
             </main>
 
         </>
