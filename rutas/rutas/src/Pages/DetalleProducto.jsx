18361db945ab303a116e4a03c27b35af12e40f35
Antonio Espinola Ramirez <antonioespinola344@gmail.com>
2026-05-11 18:02:42 +0200

    Estilos

--- rutas/rutas/src/Pages/DetalleProducto.jsx
+++ rutas/rutas/src/Pages/DetalleProducto.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useParams, useNavigate } from "react-router-dom"
 import { productos } from '../Productos/Productos'
+import './DetalleProducto.css'
 
 function DetalleProducto() {
     const { id } = useParams();
--- rutas/rutas/src/Productos/Productos.js
+++ rutas/rutas/src/Productos/Productos.js
@@ -3,42 +3,42 @@ export const productos = [
         "id": 1,
         "nombre": "NVIDIA RTX 4080 Super",
         "precio": 1200,
-        "imagen": "https://m.media-amazon.com/images/I/719uS9rfS2L.jpg",
+        "imagen": "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcTivhvtWHPl7hdiDNmULvtMSqHr9qKoxc0ZmKH4hiU5K9qEfhltALPIQJ_kbeF2dDrgtbg6cCtB8CteM7U",
         "descripcion": "Domina cualquier juego en 4K con trazado de rayos de última generación y tecnología DLSS 3.5."
     },
     {
         "id": 2,
         "nombre": "AMD Ryzen 7 7800X3D",
         "precio": 450,
-        "imagen": "https://m.media-amazon.com/images/I/51fS8q81TXL.jpg",
+        "imagen": "https://encrypted-tbn3.gstatic.com/licensed-image?q=tbn:ANd9GcRZ-oQzmHf1mfOqmTvzpDVyAM1Er6t3_XMhRskmja5ofO8Jzr5mDqSCOxD6F24fYXrzCgQBCvJrWq02Vjw",
         "descripcion": "El mejor procesador para gaming del mercado gracias a su tecnología 3D V-Cache de alto rendimiento."
     },
     {
         "id": 3,
         "nombre": "G.Skill Trident Z5 RGB 32GB",
         "precio": 130,
-        "imagen": "https://m.media-amazon.com/images/I/61S8YqS7S7L.jpg",
+        "imagen": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcSsleDVJ5pKURuLDPruOcwyQYsKLU2jxc_vtOhKQPc4ombwFg3Qd3nDTwahzltSOeLtijRhOJ4EWSF_4b0",
         "descripcion": "Memoria DDR5 de alta velocidad con iluminación RGB personalizable para una estética impecable."
     },
     {
         "id": 4,
         "nombre": "ASUS ROG Swift 27\" OLED",
         "precio": 900,
-        "imagen": "https://m.media-amazon.com/images/I/71pMv8h9S6L.jpg",
+        "imagen": "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcQzwELdJ5NZg5MqJGlrBw2mKdaH8V_lizS_5VKYG4cWBbMOj8xMq66OEnGY7HAfSDCKvMlVtP-_ozhe8XI",
         "descripcion": "Monitor gaming con panel OLED de 240Hz, ofreciendo los negros más puros y tiempos de respuesta instantáneos."
     },
     {
         "id": 5,
         "nombre": "Samsung 990 Pro 2TB",
         "precio": 180,
-        "imagen": "https://m.media-amazon.com/images/I/61NfC9vQpWL.jpg",
+        "imagen": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQdfziT4hv-4uyME9htXoTz4uRV7SW0GADtYkOnkt21eIUx1CAD7DBgVmihlOZaVyAAY_vm5RXwnoiqagA",
         "descripcion": "SSD NVMe PCIe 4.0 con velocidades de lectura de hasta 7450 MB/s para cargas de juego ultrarrápidas."
     },
     {
         "id": 6,
         "nombre": "Lian Li PC-O11 Dynamic",
         "precio": 150,
-        "imagen": "https://m.media-amazon.com/images/I/71LpMhS0fAL.jpg",
+        "imagen": "https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcQP9Q3VEEMdsEE9EcErtcAj9T9gmzh6jhkeaYQVsRJQD22fLjhpaZmWJ2ePq_L2DI6ay7pmlT1A0KfCApA",
         "descripcion": "El gabinete icónico con diseño de doble cámara y cristal templado, ideal para setups de exhibición."
     }
 ];
