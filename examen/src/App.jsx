1c373932df1c4e3c97d5223856e50f2575aed5c0
Antonio Espinola Ramirez <antonioespinola344@gmail.com>
2026-05-14 14:13:06 +0200

    Ejercicio 2

--- examen/src/App.jsx
+++ examen/src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 import UserCard from './componentes/UserCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // Variables Contador Ejercicio 2
+  const [count, setCount] = useState(0) // Contador
+  const color = count > 0 ? "green" : count < 0 ? "red" : "black" // Condicion Color
 
   return (
     <>
@@ -16,6 +19,27 @@ function App() {
       <UserCard nombre={"antonio"} email={"antonioHernandez123@gmail.com"} edad={12}></UserCard>
       <UserCard nombre={"maria"} email={"mariaHernandez123@gmail.com"} edad={15}></UserCard>
       <UserCard nombre={"juan"} email={"juanHernandez123@gmail.com"} edad={20}></UserCard>
+
+      {/**
+       * Ejercicio 2
+       * Un contador con dos botones (sumar y restar)
+       * Condicion de color:
+       * Positivo: green  Negativo: red  0: Black       
+       */}
+
+      <button type="button" className="counter" onClick={() => setCount((count) => count + 1)}>
+        sumar
+      </button>
+
+
+      <button type="button" className="counter" onClick={() => setCount((count) => count - 1)}>
+        Restar
+      </button>
+
+      <p style={{ border: "1px solid white", color}}>{count}</p>
+
+
+
     </>
   )
 }
